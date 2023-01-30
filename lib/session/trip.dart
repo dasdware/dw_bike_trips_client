069fc8d1e853fe019b2c1ddac50a22ae507e3907
dasdware <info@dasdware.de>
2023-01-30 20:59:40 +0100

    #12 Added changeable trips

--- lib/session/trip.dart
+++ lib/session/trip.dart
@@ -1,7 +1,29 @@
+import 'dart:async';
+
 class Trip {
   final int id;
   final DateTime timestamp;
   final double distance;
 
   Trip({this.id = -1, this.timestamp, this.distance});
+}
+
+class ChangeableTrip {
+  Trip _trip;
+
+  ChangeableTrip(this._trip);
+
+  Trip get trip => _trip;
+  int get id => _trip.id;
+  DateTime get timestamp => _trip.timestamp;
+  double get distance => _trip.distance;
+
+  final StreamController<Trip> _streamController =
+      StreamController<Trip>.broadcast();
+  Stream<Trip> get stream => _streamController.stream;
+
+  void update(DateTime timestamp, double distance) {
+    _trip = Trip(id: _trip.id, timestamp: timestamp, distance: distance);
+    _streamController.sink.add(_trip);
+  }
 }
